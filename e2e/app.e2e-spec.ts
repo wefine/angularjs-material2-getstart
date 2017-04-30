f3940bd163012ffc2fda55a5af70eed4eb13d751
Angular CLI <angular-cli@angular.io>
2017-04-30 10:30:44 +0800

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AngularjsMaterial2GetstartPage } from './app.po';
+
+describe('angularjs-material2-getstart App', () => {
+  let page: AngularjsMaterial2GetstartPage;
+
+  beforeEach(() => {
+    page = new AngularjsMaterial2GetstartPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
